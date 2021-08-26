9d95f662221c2caba1a1d58efdc67c298733ecde
Ludio22 <62996674+Ludio22@users.noreply.github.com>
2021-08-26 18:52:45 +0300

    books-block

---
+++ src/components/books-container/BooksContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Preloader from '../Preloader';
+
+const BooksContainer = (props) => {
+    return(
+        <div className="search__results">
+            {!props.isEmpty && <h2 className="search__total">Найдено книг: {props.totalBook}</h2> }
+            <div className="search__books" style={{display: "flex", flexWrap: "wrap"}}>
+                {props.books}
+            </div>
+            {props.isLoad && <Preloader />}
+            {!props.isEmpty && <input type="button" onClick={props.loadMore} value="Load more"/>}
+        </div>
+    );
+}
+
+export default BooksContainer;
