ec99ec20d54c92a560aadd5b367e685853d37320
Ludio22 <62996674+Ludio22@users.noreply.github.com>
2021-08-26 18:47:10 +0300

    basic-structure-app-created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import SearchPage from './components/SearchPage';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/" render={() => <SearchPage />} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
