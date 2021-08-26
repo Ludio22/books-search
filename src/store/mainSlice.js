1f6b44f367c2cdbc28e8af4e86cbdada0fa05c62
Ludio22 <62996674+Ludio22@users.noreply.github.com>
2021-08-26 18:55:14 +0300

    current-slice-with-book-data-and-metadata

---
+++ src/store/mainSlice.js
@@ -0,0 +1,76 @@
+import {
+    createAsyncThunk,
+    createSlice
+} from "@reduxjs/toolkit";
+import * as axios from 'axios';
+
+const apiKey = "AIzaSyBYS4gSAO3fIp7GJL7uR65hug_Eaz3Iu2s";
+
+export const getBooks = createAsyncThunk(
+    'main-slice/getBooks',
+    async (params) => {
+        let items;
+
+        await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${params.subject}:${params.name.replace(/ /g,'+')}&startIndex=${params.index}&maxResults=30&orderBy=${params.order}&key=${apiKey}`)
+        .then(resp => {
+            items = resp.data;
+        })        
+        return(items);
+    }
+);
+
+export const MainSlice = createSlice({
+    name: "main-slice",
+    initialState: {
+        loading: false,
+        bookName: "",
+        data: [],
+        totalBook: 0,
+        curPage: 0,
+        order: "relevance",
+        category: "all",
+        orderOptions: ["relevance", "newest"],
+        categoryOptions: ["all", "art", "biography", "computers", "history", "medical", "poetry"],
+    },
+    reducers: {
+        setBookName: (state, action) => {
+            state.bookName = action.payload;
+        },
+        clearData: (state) => {
+            state.data = [];
+        },
+        setOrder: (state, action) => {
+            state.order = action.payload;
+        },
+        setCategory: (state, action) => {
+            state.category = action.payload;
+        },
+        zeroPages: (state) => {
+            state.curPage = 0;
+        }
+    },
+    extraReducers: {
+        [getBooks.pending]: (state) => {
+            state.loading = true;
+        },
+        [getBooks.fulfilled]: (state, action) => {
+            action.payload.items.forEach(element => {
+                state.data.push(element);
+                console.log(element.volumeInfo.publishedDate);
+            });
+            state.totalBook = action.payload.totalItems;
+            state.curIndex++;
+            state.loading = false;
+        }
+    },
+});
+
+export const {
+    setBookName,
+    clearData,
+    setOrder,
+    setCategory,
+    zeroPages,
+} = MainSlice.actions;
+
+export default MainSlice.reducer;
