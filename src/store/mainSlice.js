f27010b651c7a395f890f39c5675f6c65f5bf8a9
Ludio22 <62996674+Ludio22@users.noreply.github.com>
2021-08-28 12:09:25 +0300

    added-error-handling

--- src/store/mainSlice.js
+++ src/store/mainSlice.js
@@ -8,14 +8,37 @@ const apiKey = "AIzaSyBYS4gSAO3fIp7GJL7uR65hug_Eaz3Iu2s";
 
 export const getBooks = createAsyncThunk(
     'main-slice/getBooks',
-    async (params) => {
-        let items;
+    async (params, {rejectWithValue}) => {
+        try {    
+            let items;
 
-        await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${params.subject}:${params.name.replace(/ /g,'+')}&startIndex=${params.index}&maxResults=30&orderBy=${params.order}&key=${apiKey}`)
-        .then(resp => {
-            items = resp.data;
-        })        
-        return(items);
+            await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${params.subject}:${params.name.replace(/ /g,'+')}&startIndex=${params.index}&maxResults=30&orderBy=${params.order}&key=${apiKey}`)
+            .then(resp => {
+                items = resp.data;
+            })        
+
+            return items;
+        } catch (error) {
+            return rejectWithValue(error.message);
+        }
+    }
+);
+
+export const getCurBook = createAsyncThunk(
+    'main-slice/getCurBook',
+    async (id, {rejectWithValue}) => {
+        try {
+            let item;
+    
+            await axios.get(`https://www.googleapis.com/books/v1/volumes/${id}`)
+            .then(resp => {
+                item = resp.data;
+            })        
+    
+            return(item);
+        } catch (error) {
+            return rejectWithValue(error.message);
+        }
     }
 );
 
@@ -23,8 +46,10 @@ export const MainSlice = createSlice({
     name: "main-slice",
     initialState: {
         loading: false,
+        error: null,
         bookName: "",
         data: [],
+        curBook: null,
         totalBook: 0,
         curPage: 0,
         order: "relevance",
@@ -51,17 +76,34 @@ export const MainSlice = createSlice({
     },
     extraReducers: {
         [getBooks.pending]: (state) => {
+            state.error = null;
             state.loading = true;
         },
         [getBooks.fulfilled]: (state, action) => {
+            state.curBook = null;
             action.payload.items.forEach(element => {
                 state.data.push(element);
-                console.log(element.volumeInfo);
             });
             state.totalBook = action.payload.totalItems;
             state.curPage += 30;
             state.loading = false;
-        }
+        },
+        [getBooks.rejected]: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
+        [getCurBook.pending]: (state) => {
+            state.error = null;
+            state.loading = true;
+        },
+        [getCurBook.fulfilled]: (state, action) => {
+            state.curBook = action.payload;
+            state.loading = false;
+        },
+        [getCurBook.rejected]: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
     },
 });
 
