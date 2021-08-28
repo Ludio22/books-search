1e94c9f0f2d72c49893d21849f2e23a041c9410f
Ludio22 <62996674+Ludio22@users.noreply.github.com>
2021-08-28 12:08:43 +0300

    added-links

--- src/components/books-container/BookCard.jsx
+++ src/components/books-container/BookCard.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const BookCard = (props) => {
     return(
         <div className="search__book-card">
           <div className="wrapper">
-              <img src={props.img} alt="book" />
-              <div className="search__book-info">
-                    <p className="search__book-name">{props.name}</p>
-                    <p className="search__book-categories">{props.categories}</p>
-                    <p className="search__book-autors">{(props.authors !== undefined) ? 
-                        props.authors.map(el => <span key={el.index}>{el}</span>)
-                        :
-                        null
-                        }</p>
-              </div>
+                {(props.img !== null) ?
+                <div className="img__wrapper">
+                    <Link to={"/" + props.id}>
+                        <img src={props.img} alt="book" />
+                    </Link>
+                </div>
+                :
+                <Link to={"/" + props.id}>          
+                    <img src="https://bitsofco.de/content/images/2018/12/Screenshot-2018-12-16-at-21.06.29.png" alt="book" />
+                </Link>
+                }
+                <h3 className="search__book-name">{props.name}</h3>
+                <p className="search__book-categories">{props.categories}</p>
+                <p className="search__book-autors">
+                    {(props.authors !== undefined) ? 
+                    props.authors.map(el => <span>{el}<br /></span>)
+                    :
+                    null
+                    }
+                </p>
           </div>
         </div>
     );
